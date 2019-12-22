be16f11fe838ad8b756dbb60b95acdcc908bdb54
adshes <42263959+adshes@users.noreply.github.com>
2019-12-22 21:33:19 +0300

    Initial commit

---
+++ src/test14.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class test14 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String S = in.nextLine();
+        System.out.println(S);
+        int X = Integer.parseInt(S);
+        System.out.println(X);
+        double Y = X;
+        System.out.println(Y);
+    }
+}
